39a29c36f7e9d275eaa43639f2b7e81bd6ee7405
Christopher Smith <nullst8@ChristohersMBP2.socal.rr.com>
2024-01-05 17:27:28 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='font-sans text-white w-100'>
+      <header className='  p-3 bg-black w-100'>
+        <h1 className='float-left text-xl tracking-wider text-lime-500'>
+          projector
+        </h1>
+        <nav className='display-block align-right'>
+          <ul className='display-inline'>
+            <li className='float-right pr-4'>about</li>
+            <li className='float-right pr-4'>about2</li>
+            <li className='float-right pr-4'>about3</li>
+          </ul>
+        </nav>
+      </header>
+      <section className='pt-9'></section>
+    </div>
+  );
+}
+
+export default App;
